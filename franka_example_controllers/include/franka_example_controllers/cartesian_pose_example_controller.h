65c703407533b097ba66f9747d0518a214dd6bf2
mahdinobar <nobar.mahdi@gmail.com>
2024-05-09 14:38:29 +0200

    linear interpolation added for cartesian position reaching controller

--- franka_example_controllers/include/franka_example_controllers/cartesian_pose_example_controller.h
+++ franka_example_controllers/include/franka_example_controllers/cartesian_pose_example_controller.h
@@ -13,22 +13,42 @@
 #include <ros/time.h>
 
 #include <franka_hw/franka_cartesian_command_interface.h>
+#include <vector>
+
 
 namespace franka_example_controllers {
 
-class CartesianPoseExampleController
-    : public controller_interface::MultiInterfaceController<franka_hw::FrankaPoseCartesianInterface,
-                                                            franka_hw::FrankaStateInterface> {
- public:
-  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
-  void starting(const ros::Time&) override;
-  void update(const ros::Time&, const ros::Duration& period) override;
+    class CartesianPoseExampleController
+            : public controller_interface::MultiInterfaceController<franka_hw::FrankaPoseCartesianInterface,
+                    franka_hw::FrankaStateInterface> {
+    public:
+        bool init(hardware_interface::RobotHW *robot_hardware, ros::NodeHandle &node_handle) override;
 
- private:
-  franka_hw::FrankaPoseCartesianInterface* cartesian_pose_interface_;
-  std::unique_ptr<franka_hw::FrankaCartesianPoseHandle> cartesian_pose_handle_;
-  ros::Duration elapsed_time_;
-  std::array<double, 16> initial_pose_{};
-};
+        void starting(const ros::Time &) override;
+
+        void update(const ros::Time &, const ros::Duration &period) override;
+
+    private:
+        franka_hw::FrankaPoseCartesianInterface *cartesian_pose_interface_;
+        std::unique_ptr <franka_hw::FrankaCartesianPoseHandle> cartesian_pose_handle_;
+        ros::Duration elapsed_time_;
+        std::array<double, 16> initial_pose_{};
+        std::array<double, 3> p_ee_target{{0.45039319, -0.09860044, 0.17521834}};
+        int step_k = 0;
+        std::vector<double> interp_x;
+        std::vector<double> interp_y;
+        std::vector<double> interp_z;
+        std::vector<double> linspace(double min, double max, int n) {
+            std::vector<double> result;
+            int iterator = 0;
+            for (int i = 0; i <= n - 2; i++) {
+                double temp = min + i * (max - min) / (floor((double) n) - 1);
+                result.insert(result.begin() + iterator, temp);
+                iterator += 1;
+            }
+            result.insert(result.begin() + iterator, max);
+            return result;
+        }
+    };
 
 }  // namespace franka_example_controllers
--- franka_example_controllers/src/cartesian_pose_example_controller.cpp
+++ franka_example_controllers/src/cartesian_pose_example_controller.cpp
@@ -13,81 +13,113 @@
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 
+#include <vector>
+
 namespace franka_example_controllers {
 
-bool CartesianPoseExampleController::init(hardware_interface::RobotHW* robot_hardware,
-                                          ros::NodeHandle& node_handle) {
-  cartesian_pose_interface_ = robot_hardware->get<franka_hw::FrankaPoseCartesianInterface>();
-  if (cartesian_pose_interface_ == nullptr) {
-    ROS_ERROR(
-        "CartesianPoseExampleController: Could not get Cartesian Pose "
-        "interface from hardware");
-    return false;
-  }
+    bool CartesianPoseExampleController::init(hardware_interface::RobotHW *robot_hardware,
+                                              ros::NodeHandle &node_handle) {
+        cartesian_pose_interface_ = robot_hardware->get<franka_hw::FrankaPoseCartesianInterface>();
+        if (cartesian_pose_interface_ == nullptr) {
+            ROS_ERROR(
+                    "CartesianPoseExampleController: Could not get Cartesian Pose "
+                    "interface from hardware");
+            return false;
+        }
 
-  std::string arm_id;
-  if (!node_handle.getParam("arm_id", arm_id)) {
-    ROS_ERROR("CartesianPoseExampleController: Could not get parameter arm_id");
-    return false;
-  }
+        std::string arm_id;
+        if (!node_handle.getParam("arm_id", arm_id)) {
+            ROS_ERROR("CartesianPoseExampleController: Could not get parameter arm_id");
+            return false;
+        }
 
-  try {
-    cartesian_pose_handle_ = std::make_unique<franka_hw::FrankaCartesianPoseHandle>(
-        cartesian_pose_interface_->getHandle(arm_id + "_robot"));
-  } catch (const hardware_interface::HardwareInterfaceException& e) {
-    ROS_ERROR_STREAM(
-        "CartesianPoseExampleController: Exception getting Cartesian handle: " << e.what());
-    return false;
-  }
+        try {
+            cartesian_pose_handle_ = std::make_unique<franka_hw::FrankaCartesianPoseHandle>(
+                    cartesian_pose_interface_->getHandle(arm_id + "_robot"));
+        } catch (const hardware_interface::HardwareInterfaceException &e) {
+            ROS_ERROR_STREAM(
+                    "CartesianPoseExampleController: Exception getting Cartesian handle: " << e.what());
+            return false;
+        }
 
-  auto state_interface = robot_hardware->get<franka_hw::FrankaStateInterface>();
-  if (state_interface == nullptr) {
-    ROS_ERROR("CartesianPoseExampleController: Could not get state interface from hardware");
-    return false;
-  }
+        auto state_interface = robot_hardware->get<franka_hw::FrankaStateInterface>();
+        if (state_interface == nullptr) {
+            ROS_ERROR("CartesianPoseExampleController: Could not get state interface from hardware");
+            return false;
+        }
 
-  try {
-    auto state_handle = state_interface->getHandle(arm_id + "_robot");
+        try {
+            auto state_handle = state_interface->getHandle(arm_id + "_robot");
 
-    std::array<double, 7> q_start{{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-    for (size_t i = 0; i < q_start.size(); i++) {
-      if (std::abs(state_handle.getRobotState().q_d[i] - q_start[i]) > 0.1) {
-        ROS_ERROR_STREAM(
-            "CartesianPoseExampleController: Robot is not in the expected starting position for "
-            "running this example. Run `roslaunch franka_example_controllers move_to_start.launch "
-            "robot_ip:=<robot-ip> load_gripper:=<has-attached-gripper>` first.");
-        return false;
-      }
+//            std::array<double, 7> q_start{{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+            std::array<double, 7> q_start{
+                    {-0.42529795, 0.11298615, 0.20446317, -2.52843438, -0.15231932, 2.63230466, M_PI_4}};
+            for (size_t i = 0; i < q_start.size(); i++) {
+                std::cout << "++++++++state_handle.getRobotState().q_d[i]=" << state_handle.getRobotState().q_d[i]
+                          << " \n";
+                std::cout << "--------q_start[i]=" << q_start[i] << " \n";
+                if (std::abs(state_handle.getRobotState().q_d[i] - q_start[i]) > 0.1) {
+                    ROS_ERROR_STREAM(
+                            "CartesianPoseExampleController: Robot is not in the expected starting position for "
+                            "running this example. Run `roslaunch franka_example_controllers move_to_start.launch "
+                            "robot_ip:=<robot-ip> load_gripper:=<has-attached-gripper>` first.");
+                    return false;
+                }
+            }
+        } catch (const hardware_interface::HardwareInterfaceException &e) {
+            ROS_ERROR_STREAM(
+                    "CartesianPoseExampleController: Exception getting state handle: " << e.what());
+            return false;
+        }
+
+        return true;
     }
-  } catch (const hardware_interface::HardwareInterfaceException& e) {
-    ROS_ERROR_STREAM(
-        "CartesianPoseExampleController: Exception getting state handle: " << e.what());
-    return false;
-  }
 
-  return true;
-}
+    void CartesianPoseExampleController::starting(const ros::Time & /* time */) {
+        initial_pose_ = cartesian_pose_handle_->getRobotState().O_T_EE_d;
+//        std::vector<double> linspace(double min, double max, int n) {
+//            vector<double> result;
+//            int iterator = 0;
+//            for (int i = 0; i <= n - 2; i++) {
+//                double temp = min + i * (max - min) / (floor((double) n) - 1);
+//                result.insert(result.begin() + iterator, temp);
+//                iterator += 1;
+//            }
+//            result.insert(result.begin() + iterator, max);
+//            return result;
+//        }
+        interp_x = linspace(initial_pose_[12], p_ee_target[0], 100);
+        interp_y = linspace(initial_pose_[13], p_ee_target[1], 100);
+        interp_z = linspace(initial_pose_[14], p_ee_target[2], 100);
+        elapsed_time_ = ros::Duration(0.0);
+    }
 
-void CartesianPoseExampleController::starting(const ros::Time& /* time */) {
-  initial_pose_ = cartesian_pose_handle_->getRobotState().O_T_EE_d;
-  elapsed_time_ = ros::Duration(0.0);
-}
+    void CartesianPoseExampleController::update(const ros::Time & /* time */,
+                                                const ros::Duration &period) {
+        elapsed_time_ += period;
 
-void CartesianPoseExampleController::update(const ros::Time& /* time */,
-                                            const ros::Duration& period) {
-  elapsed_time_ += period;
-
-  double radius = 0.15;
-  double angle = M_PI / 4 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec()));
-  double delta_x = radius * std::sin(angle);
-  double delta_z = radius * (std::cos(angle) - 1);
-  std::array<double, 16> new_pose = initial_pose_;
-  new_pose[12] -= delta_x;
-  new_pose[14] -= delta_z;
-  cartesian_pose_handle_->setCommand(new_pose);
-}
+        double radius = 0.15;
+        double angle = M_PI / 4 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec()));
+        double delta_x = radius * std::sin(angle);
+        double delta_z = radius * (std::cos(angle) - 1);
+        std::array<double, 16> new_pose = initial_pose_;
+//        new_pose[12] -= delta_x;
+//        new_pose[14] -= delta_z;
+        if (step_k < 100) {
+            new_pose[12] = interp_x[step_k];
+            new_pose[13] = interp_y[step_k];
+            new_pose[14] = interp_z[step_k];
+        } else {
+            new_pose[12] = 0.45039319;
+            new_pose[13] = -0.09860044;
+            new_pose[14] = 0.17521834;
+        }
+        cartesian_pose_handle_->setCommand(new_pose);
+        step_k += 1;
+    }
 
 }  // namespace franka_example_controllers
 
 PLUGINLIB_EXPORT_CLASS(franka_example_controllers::CartesianPoseExampleController,
-                       controller_interface::ControllerBase)
+        controller_interface::ControllerBase
+)
