efe23a471933d606e2cf3a65954d3b172be4d92b
mahdinobar <nobar.mahdi@gmail.com>
2025-06-22 22:35:47 +0200

    setting 6 + lambda pseudo inv corrected, changed Analytical Pinv

--- franka_example_controllers/include/franka_example_controllers/pseudo_inversion.h
+++ franka_example_controllers/include/franka_example_controllers/pseudo_inversion.h
@@ -13,18 +13,33 @@
 
 namespace franka_example_controllers {
 
-inline void pseudoInverse(const Eigen::MatrixXd& M_, Eigen::MatrixXd& M_pinv_, bool damped = true) {
-  double lambda_ = damped ? 0.01 : 0.0;
+//inline void pseudoInverse(const Eigen::MatrixXd& M_, Eigen::MatrixXd& M_pinv_, bool damped = true) {
+//  double lambda_ = damped ? 0.01 : 0.0;
+//
+//  Eigen::JacobiSVD<Eigen::MatrixXd> svd(M_, Eigen::ComputeFullU | Eigen::ComputeFullV);
+//  Eigen::JacobiSVD<Eigen::MatrixXd>::SingularValuesType sing_vals_ = svd.singularValues();
+//  Eigen::MatrixXd S_ = M_;  // copying the dimensions of M_, its content is not needed.
+//  S_.setZero();
+//
+//  for (int i = 0; i < sing_vals_.size(); i++)
+//    S_(i, i) = (sing_vals_(i)) / (sing_vals_(i) * sing_vals_(i) + lambda_ * lambda_);
+//
+//  M_pinv_ = Eigen::MatrixXd(svd.matrixV() * S_.transpose() * svd.matrixU().transpose());
+//}
+inline void pseudoInverseAnalytical(const Eigen::MatrixXd& A, Eigen::MatrixXd& A_pinv, double lambda) {
+  int m = A.rows();
+  int n = A.cols();
 
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd(M_, Eigen::ComputeFullU | Eigen::ComputeFullV);
-  Eigen::JacobiSVD<Eigen::MatrixXd>::SingularValuesType sing_vals_ = svd.singularValues();
-  Eigen::MatrixXd S_ = M_;  // copying the dimensions of M_, its content is not needed.
-  S_.setZero();
-
-  for (int i = 0; i < sing_vals_.size(); i++)
-    S_(i, i) = (sing_vals_(i)) / (sing_vals_(i) * sing_vals_(i) + lambda_ * lambda_);
-
-  M_pinv_ = Eigen::MatrixXd(svd.matrixV() * S_.transpose() * svd.matrixU().transpose());
+  if (m > n) {
+    // Left pseudoinverse: (A^T A + λ² I)^-1 A^T
+    Eigen::MatrixXd AtA = A.transpose() * A;
+    Eigen::MatrixXd damping = lambda * lambda * Eigen::MatrixXd::Identity(n, n);
+    A_pinv = (AtA + damping).ldlt().solve(A.transpose());
+  } else {
+    // Right pseudoinverse: A^T (A A^T + λ² I)^-1
+    Eigen::MatrixXd AAt = A * A.transpose();
+    Eigen::MatrixXd damping = lambda * lambda * Eigen::MatrixXd::Identity(m, m);
+    A_pinv = A.transpose() * (AAt + damping).ldlt().solve(Eigen::MatrixXd::Identity(m, m));
+  }
 }
-
 }  // namespace franka_example_controllers
--- franka_example_controllers/src/PRIMITIVE_velocity_controller.cpp
+++ franka_example_controllers/src/PRIMITIVE_velocity_controller.cpp
@@ -824,7 +824,10 @@ void PRIMITIVEVelocityController::update(const ros::Time& rosTime, const ros::Du
               K_i * I_e[i];  //+ K_i * np.sum(e[:,1:],1)*dt_fast + K_d*(v_ref-v_e)
     }
     Eigen::MatrixXd J_translation_pinv;
-    pseudoInverse(J_translation, J_translation_pinv);
+//    pseudoInverse(J_translation, J_translation_pinv);
+    double damping_lambda = 0.01;
+    pseudoInverseAnalytical(J_translation, J_translation_pinv, damping_lambda);
+
 
     dq_command_PID = J_translation_pinv * vc;
   } else if (start_up == false) {
@@ -872,7 +875,9 @@ void PRIMITIVEVelocityController::update(const ros::Time& rosTime, const ros::Du
       pinocchio::computeFrameJacobian(model_pino_biased, data_pino, q_extended, 26,
                                       pinocchio::ReferenceFrame::LOCAL_WORLD_ALIGNED, jacobian_tmp);
       Eigen::MatrixXd J_translation_biased = jacobian_tmp.block(0, 0, 3, 7);
-      pseudoInverse(J_translation_biased, J_translation_pinv_biased);
+//      pseudoInverse(J_translation_biased, J_translation_pinv_biased);
+      double damping_lambda = 0.01;
+      pseudoInverseAnalytical(J_translation_biased, J_translation_pinv_biased, damping_lambda);
       dq_command_PID = J_translation_pinv_biased * vc;
 
       //      if (false) {
